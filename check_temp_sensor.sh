b90f67a881c83dd1c148388ab0962c3bb5c27e61
charkster <31560568+charkster@users.noreply.github.com>
2025-09-03 17:54:40 -0700

    Create check_temp_sensor.sh

---
+++ check_temp_sensor.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if curl -s --max-time 3 --head  --request GET http://192.168.0.201 | grep "200 OK" > /dev/null; then 
+   echo "sensor is UP"
+else
+  RECIPIENT="YOUR_EMAIL@gmail.com"
+  SUBJECT="Temp sensor down"
+  BODY="\n$(date)"
+  echo -e "Subject: $SUBJECT\n\n$BODY" | msmtp --account=default "$RECIPIENT"
+  echo "sensor is DOWN"
+fi
