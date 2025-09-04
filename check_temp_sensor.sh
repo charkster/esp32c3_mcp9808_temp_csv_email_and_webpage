c732b6da7930df9f156cc03d9ef848291fe5ca98
charkster <31560568+charkster@users.noreply.github.com>
2025-09-03 17:55:10 -0700

    Update check_temp_sensor.sh

--- check_temp_sensor.sh
+++ check_temp_sensor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if curl -s --max-time 3 --head  --request GET http://192.168.0.201 | grep "200 OK" > /dev/null; then 
+if curl -s --max-time 3 --head  --request GET http://192.168.0.203 | grep "200 OK" > /dev/null; then 
    echo "sensor is UP"
 else
   RECIPIENT="YOUR_EMAIL@gmail.com"
